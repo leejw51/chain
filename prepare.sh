5ebae59a0c2c04fe1afe0f471cb16ed4d0fc79c6
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 18:19:12 +0800

    pkg config

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ rm -rf /var/lib/apt/lists/*
 apt update
 apt install curl -y
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
+apt install pkg-config -y
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "prepare OK"
