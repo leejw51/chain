3cd8cab3377a451a43b0178e91f3b25ed5df11fd
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 16:41:14 +0800

    install files

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cat /etc/issue
 export RUST_BACKTRACE=1
 export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
 export PATH=$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
+sudo apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
 echo "prepare OK"
