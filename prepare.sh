2b6f4ca000094a3b2e26d3f39d784500ba08fff5
Jongwhan Lee <leejw51@gmail.com>
2019-10-11 21:49:18 +0800

    install cmake3

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,8 @@ sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
 rm -rf /var/lib/apt/lists/*
 apt update
 apt upgrade
+apt remove cmake -y
+apt install cmake -y
 apt install wget curl -y
 apt install python3 libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 apt install pkg-config -y
