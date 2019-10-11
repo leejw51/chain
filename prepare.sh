6a3e764aa44d2bab409efc2506511dd87b5de62f
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:41:44 +0800

    env

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 cat /etc/issue
-export RUST_BACKTRACE=1
-export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
-export PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH
-export LD_LIBRARY_PATH=$HOME/lib
-export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
-
-
 sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
 rm -rf /var/lib/apt/lists/*
 apt update
@@ -14,4 +7,4 @@ apt install curl -y
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-echo "prepare OK2"
+echo "prepare OK"
