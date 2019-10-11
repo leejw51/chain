6bdf0966ee91f0fc53b34622f4999eb8c7c5ff5d
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 16:41:38 +0800

    app update

--- prepare.sh
+++ prepare.sh
@@ -5,5 +5,6 @@ export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
 export PATH=$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
+sudo apt update
 sudo apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
 echo "prepare OK"
