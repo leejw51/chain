5108925e47926dba51a574d9bc52625122b3e369
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:10:14 +0800

    OK2

--- prepare.sh
+++ prepare.sh
@@ -7,4 +7,4 @@ export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
-echo "prepare OK"
+echo "prepare OK2"
