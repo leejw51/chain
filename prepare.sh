f96b9e6ef43ae74fbfa4d409566bc0e5158f6e0d
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 16:59:50 +0800

    docker chain

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,5 @@ export PATH=$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-apt update
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
 echo "prepare OK"
