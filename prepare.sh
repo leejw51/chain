cd3ff1bcf9b3e8aebc6212c5e49dd77799cdbc3c
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 16:37:11 +0800

    export

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+export RUST_BACKTRACE=1
+export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
+export PATH=$HOME/.local/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/lib
+export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 echo "prepare OK"
