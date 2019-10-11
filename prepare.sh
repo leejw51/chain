5578072c9b48c4b1ba02725cda0cd4250977502c
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:22:35 +0800

    automatic

--- prepare.sh
+++ prepare.sh
@@ -11,7 +11,7 @@ sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
 rm -rf /var/lib/apt/lists/*
 apt update
 apt install curl
-apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
+apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "prepare OK2"
