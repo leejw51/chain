af8bd22e430fc8791655def84689073be8f0be07
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:23:56 +0800

    install curl

--- prepare.sh
+++ prepare.sh
@@ -10,7 +10,7 @@ export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
 rm -rf /var/lib/apt/lists/*
 apt update
-apt install curl
+apt install curl -y
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
