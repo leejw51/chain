c7fb0df76c2e50c3c2bf136fdfc6ff0019172acd
Jongwhan Lee <leejw51@gmail.com>
2019-10-11 21:59:49 +0800

    a

--- prepare.sh
+++ prepare.sh
@@ -2,10 +2,9 @@
 cat /etc/issue
 sed -i 's/archive.ubuntu.com/ftp.daum.net/g' /etc/apt/sources.list
 rm -rf /var/lib/apt/lists/*
+apt-get install software-properties-common -y
+add-apt-repository ppa:george-edison55/cmake-3.x -y
 apt update
-apt upgrade
-apt remove cmake -y
-apt install cmake -y
 apt install wget curl -y
 apt install python3 libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 apt install pkg-config -y
