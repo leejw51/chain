d21175466263fa703ce20ad2b7d854ffdb822dac
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:13:22 +0800

    remove cache

--- prepare.sh
+++ prepare.sh
@@ -6,5 +6,9 @@ export PATH=$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 
+
+rm -rf /var/lib/apt/lists/*
+apt update
+apt install curl
 apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev
 echo "prepare OK2"
