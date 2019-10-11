f3f263bddc4254a6c91b7f72c26a36cb7c974ffd
Jongwhan Lee <leejw51@gmail.com>
2019-10-11 21:47:21 +0800

    add python3

--- prepare.sh
+++ prepare.sh
@@ -5,7 +5,7 @@ rm -rf /var/lib/apt/lists/*
 apt update
 apt upgrade
 apt install wget curl -y
-apt install libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
+apt install python3 libssl-dev libcurl4-openssl-dev libelf-dev libdw-dev cmake gcc binutils-dev libc6-dev -y
 apt install pkg-config -y
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
