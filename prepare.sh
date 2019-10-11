af16120cbec73944937d0108f6975878c9d1e7ee
Jongwhan Lee <jonghwan@crypto.com>
2019-10-11 17:27:27 +0800

    cargo bin

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,7 @@
 cat /etc/issue
 export RUST_BACKTRACE=1
 export RUSTFLAGS=-Ctarget-feature=+aes,+ssse3
-export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.cargo/bin:$HOME/.local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/lib
 export PKG_CONFIG_PATH=$HOME/lib/pkgconfig
 
